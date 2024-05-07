e7ab6d0645181fccd3c8a5e97408fb322aeb4d11
Ckola99 <Christopherkola@gmail.com>
2024-05-07 17:12:42 +0200

    portfolio

---
+++ portfolio_website/main.js
@@ -0,0 +1,7 @@
+ var typed = new Typed('.animated', {
+    strings: ['Full-Stack Software Engineer', 'Backend Engineer', 'Frontend Engineer'],
+    typeSpeed: 70,
+    backSpeed: 70,
+    loop: true,
+    loopCount: Infinity,
+  });
